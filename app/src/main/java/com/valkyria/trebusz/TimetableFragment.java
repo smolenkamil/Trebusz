d66f2720933a9852390331fc79b41ba115c3e10e
Kamil <pl.smolen.kamil@gmail.com>
2017-10-06 20:31:42 +0200

    fragments tabs

---
+++ app/src/main/java/com/valkyria/trebusz/TimetableFragment.java
@@ -0,0 +1,30 @@
+package com.valkyria.trebusz;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TimetableFragment extends Fragment {
+
+    public TimetableFragment() {
+    }
+
+    public static TimetableFragment newInstance() {
+        return new TimetableFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_timetable, container, false);
+    }
+
+}
