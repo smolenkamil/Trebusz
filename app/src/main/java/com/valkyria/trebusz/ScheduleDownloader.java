8dd5a395dab8127d0aeaa5dea8839d520e80e7db
Kamil <pl.smolen.kamil@gmail.com>
2017-11-03 20:53:33 +0100

    Stworzenie klasy wykonującej pobieranie planu zajec

---
+++ app/src/main/java/com/valkyria/trebusz/ScheduleDownloader.java
@@ -0,0 +1,16 @@
+package com.valkyria.trebusz;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Kamil on 03.11.2017.
+ */
+
+public class ScheduleDownloader extends AsyncTask<Void,Void,Void> {
+
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
